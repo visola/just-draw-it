d2fb8f1770fd7d8c324304201d52a6e5a33dcbb8
Vinicius Isola <viniciusisola@gmail.com>
2020-04-18 07:06:41 -0400

    Remove files from previous encarnation

--- js/models/Ellipse.js
+++
@@ -1,8 +0,0 @@
-import Drawable from './Drawable';
-import EllipseComponent from '../components/drawables/Ellipse';
-
-export default class Ellipse extends Drawable {
-  get component() {
-    return EllipseComponent;
-  }
-}
