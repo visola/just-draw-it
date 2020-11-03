547ae7f5f77a394c56dda222d36a64b898481379
Vinicius Isola <viniciusisola@gmail.com>
2020-11-02 22:34:22 -0500

    Simplify the scale ellipse code a bit

--- js/tools/scale/ellipse.js
+++ js/tools/scale/ellipse.js
@@ -8,22 +8,28 @@ function fetchCurrentState(element) {
 }
 
 function scale(element, initialState, direction, amountX, amountY) {
+  let {cx, cy, rx, ry} = initialState;
+
+  cy += amountY / 2;
+  cx += amountX / 2;
+
   if (direction.indexOf('s') >= 0) {
-    element.setAttribute('cy', initialState.cy + amountY / 2);
-    element.setAttribute('ry', initialState.ry + amountY / 2);
+    ry += amountY / 2;
   }
   if (direction.indexOf('n') >= 0) {
-    element.setAttribute('cy', initialState.cy + amountY / 2);
-    element.setAttribute('ry', initialState.ry - amountY / 2);
+    ry -= amountY / 2;
   }
   if (direction.indexOf('e') >= 0) {
-    element.setAttribute('cx', initialState.cx + amountX / 2);
-    element.setAttribute('rx', initialState.rx + amountX / 2);
+    rx += amountX / 2;
   }
   if (direction.indexOf('w') >= 0) {
-    element.setAttribute('cx', initialState.cx + amountX / 2);
-    element.setAttribute('rx', initialState.rx - amountX / 2);
+    rx -= amountX / 2;
   }
+
+  element.setAttribute('cx', cx);
+  element.setAttribute('rx', rx);
+  element.setAttribute('cy', cy);
+  element.setAttribute('ry', ry);
 }
 
 export default {
