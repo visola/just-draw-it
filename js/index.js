2392de5ef3a90efe417731cf49631d57bfd97229
Vinicius Isola <viniciusisola@gmail.com>
2020-11-02 20:49:03 -0500

    Fix lint errors

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 import './tools/index.js';
+import document from './document.js';
 
 const resizeCanvas = () => {
-    const svgEl = document.getElementById('canvas');
-    const parent = svgEl.parentElement;
-    svgEl.setAttribute('height', parent.clientHeight);
-    svgEl.setAttribute('width', parent.clientWidth - 56);
+  const svgEl = document.getElementById('canvas');
+  const parent = svgEl.parentElement;
+  svgEl.setAttribute('height', parent.clientHeight);
+  svgEl.setAttribute('width', parent.clientWidth - 56);
 };
 
 window.addEventListener('resize', resizeCanvas);
-resizeCanvas()
+resizeCanvas();
